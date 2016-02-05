03a6aedbb065c0e214265ae1b569e1bb178eefd0
MacStewart <mac.stewart@live.ca>
2016-02-05 10:40:50 -0500

    Added user key description + unknown key message fix

--- WebContent/phonebook.js
+++ WebContent/phonebook.js
@@ -36,12 +36,13 @@ angular.module('phonebook', [])
       $scope.entries = data.entries;
     }).error(function(data, status) {
       $scope.auth.valid = false;
+      $scope.attemptFailed = true;
     });
   }
 
   $scope.loadKey = function() {
+    $scope.attemptFailed = false;
     loadEntries();
-    $scope.attemptedLoad = true;
   };
   $scope.createKey = function() {
     $http.post('api/user', {}).then(function(data) {
